516707b26d08e9d243de069c8fc49b49c18361aa
saisai <you@example.com>
2021-01-03 09:47:58 +0700

    init

---
+++ python/rosettacode_org/Menu/Menu.py
@@ -0,0 +1,27 @@
+def _menu(items):
+    for indexitem in enumerate(items):
+        print(" %2i) %s" % indexitem)
+
+def _ok(reply, itemcount):
+    try:
+        n = int(reply)
+        return 0 <= n < itemcount
+    except:
+        return False
+
+def selector(items, prompt):
+    "Prompt to selecct an item from the items"
+    if not items: return ""
+    reply = -1
+    itemcount = len(items)
+    while not _ok(reply, itemcount):
+        _menu(items)
+        # Use input instead of raw_input for Python 3.x
+        reply = input(prompt).strip()
+    return items[int(reply)]
+
+
+if __name__ == '__main__':
+    items = ['fee fie', 'huff and puff', 'mirror mirror', 'tick tock']
+    item = selector(items, 'Which is from the three pigs: ')
+    print ("You chose: " + item)
