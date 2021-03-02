d5f5dd4f87e87a06e45edd35ec4424b59d292c7f
saisai <sai@gmail.com>
2021-03-02 20:25:25 +0700

    init

---
+++ python/rosettacode_org/Move-to-front_algorithm/Move-to-front_algorithm.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from string import ascii_lowercase
+
+SYMBOLTABLE = list(ascii_lowercase)
+
+print(SYMBOLTABLE)
+
+def move2front_encode(string, symboltable):
+    sequence, pad = [], symboltable[::]
+    for char in string:
+        indx = pad.index(char)
+        sequence.append(indx)
+        pad = [pad.pop(indx)] + pad
+    return sequence
+
+
+def move2front_decode(sequence, symboltable):
+    chars, pad = [], symboltable[::]
+    for indx in sequence:
+        char = pad[indx]
+        chars.append(char)
+        pad = [pad.pop(indx)] + pad
+    return ''.join(chars)
+
+if __name__ == '__main__':
+    for s in ['broood', 'bananaaa', 'hiphophiphop']:
+        encode = move2front_encode(s, SYMBOLTABLE)
+        print('%14r encodes to %r' % (s, encode), end=', ')
+        decode = move2front_decode(encode, SYMBOLTABLE)
+        print('which decodes back to %r' % decode)
+        assert s == decode, 'Whoops!'
