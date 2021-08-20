23cfd6bbbef971e745d89bb6f0b3e0d9160abeb0
saisai <sai@gmail.com>
2021-08-20 10:54:12 +0700

    init

---
+++ python/rosettacode_org/Apply_a_digital_filter_direct_form_II_transposed/Apply_a_digital_filter_direct_form_II_transposed.c
@@ -0,0 +1,20 @@
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+#define MAX_LEN 1000
+
+typedef struct{
+	float* values;
+	int size;
+}vector;
+
+vector extractVector(char* str){
+	vector coeff;
+	int i = 0, count = 1;
+	char* token;
+	i
+
+	while(str[i] != 00{	
+	}
