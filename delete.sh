fa829868f24a8e73a2ebf7ecd0d6cde1de059b7a
saisai <sai@gmail.com>
2021-09-14 11:24:41 +0700

    init

--- delete.sh
+++ delete.sh
@@ -1,8 +1,27 @@
- find . -type f -name *.pyc -exec rm -rfv {} \;
- find . -type f -name *.exe -exec rm -rfv {} \;
- find . -type f -name *.out -exec rm -rfv {} \;
- find . -type f -name *.so -exec rm -rfv {} \;
- find . -type f -name *.exe -exec rm -rfv {} \;
- find . -type f -name *.b -exec rm -rfv {} \;
- find . -type f -name *.o -exec rm -rfv {} \;
+find . -type f -name *.pyc -exec rm -rfv {} \;
+find . -type f -name *.exe -exec rm -rfv {} \;
+find . -type f -name *.out -exec rm -rfv {} \;
+find . -type f -name *.so -exec rm -rfv {} \;
+find . -type f -name *.exe -exec rm -rfv {} \;
+find . -type f -name *.b -exec rm -rfv {} \;
+find . -type f -name *.o -exec rm -rfv {} \;
 
+
+# delete executable file 
+
+function delete_exe {
+    
+    for i in $(find . -type f -not -path "./.git/*" ! -name "*.js" ! -name "*.py")
+    do
+        if [[ -x $i ]]
+        then
+            echo 'exe ' $i
+            rm -rfv $i
+        fi
+            #echo 'no exe ' $i
+        #fi
+    done
+
+}
+
+delete_exe
