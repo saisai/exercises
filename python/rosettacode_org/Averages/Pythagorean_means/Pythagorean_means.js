b74e6628a98d953e5f93de4364276e3802d8334c
saisai <sai@gmail.com>
2021-09-14 13:02:28 +0700

    init

---
+++ python/rosettacode_org/Averages/Pythagorean_means/Pythagorean_means.js
@@ -0,0 +1,66 @@
+var tt = (function () {
+  'use strict';
+
+  // arithmetic_mean :: [Number] -> Number
+  function arithmetic_mean(ns) {
+    return(
+      ns.reduce( // sum
+        function(sum, n) {
+          return (sum + n)
+
+        },
+        0      
+      ) / ns.length
+    )
+  }
+
+
+  // geometirc_mean :: [Number] -> number
+  function geometric_mean(ns) {
+    return Math.pow(
+      ns.reduce( // product
+        function (product, n) {
+          return (product * n)
+        },
+        1      
+      ),
+      1 / ns.length
+    )
+  }
+
+
+  // harmonic_mean :: [Number] -> Number
+  function harmonic_mean(ns){
+    return(
+      ns.length / ns.reduce( // sum of inverses
+        function (invSum, n) {
+          return (invSum + (1 / n));
+        },
+        0
+      )
+
+    )
+  }
+
+var values = [arithmetic_mean, geometric_mean, harmonic_mean]
+    .map(function(f){
+      return f([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+    }),
+    mean = {
+      Arithmetic: values[0], // arithmetic
+      Geometric: values[1], // geometric
+      Haromic: values[2] // harmonic
+    }
+
+return JSON.stringify({
+  values: mean,
+  test: "is A >= G >= H ? " +
+  (
+    mean.Arithemetic >= mean.Geometric &&
+    mean.Geometric >= mean.Harmonic ? "yes" : "no"
+  )
+}, null, 2);
+
+})();
+
+console.log(tt);
---
+++ python/rosettacode_org/Averages/Root_mean_square/Root_mean_square2.js
@@ -0,0 +1,20 @@
+const tt = (() => {
+
+  'use strict';
+
+  // rootMeanSquare :: [Num] -> Real
+  const rootMeanSquare = xs =>
+    Math.sqrt(
+      xs.reduce(
+        (a, x) => (a + x * x),
+        0  
+      ) / xs.length 
+    );
+
+  return rootMeanSquare([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
+
+
+})();
+
+
+console.log(tt);
