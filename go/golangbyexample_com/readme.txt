c80f2b24af719af7aaf05ed389a3fe9d2fa404ae
saisai <sai@gmail.com>
2022-05-16 14:56:28 +0700

    init

---
+++ go/golangbyexample_com/readme.txt
@@ -0,0 +1,1 @@
+https://golangbyexample.com/golang-comprehensive-tutorial/
---
+++ go/zetcode_com/golang/pipe/simple.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+func main() {
+	r, w := io.Pipe()
+
+	go func() {
+		fmt.Fprintf(w, "Hello there\n")
+		w.Close()
+	}()
+
+	_, err := io.Copy(os.Stdout, r)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
