c8dd76acf7fa3526865ca0b0979eb3feea456e7b
saisai <sai@gmail.com>
2023-01-30 20:47:34 +0700

    init

--- python/django/django-webpack/assets/js/SingInApp.jsx
+++ python/django/django-webpack/assets/js/SingInApp.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignIn from './components/SignIn'
 import SignUp from './components/SignUp';
+import SignInSide from './components/SignInSide'
 
 function SingInApp() {
   return (
     <div >
-      <SignUp />
+      <SignInSide />
     </div>
 	);
 }
