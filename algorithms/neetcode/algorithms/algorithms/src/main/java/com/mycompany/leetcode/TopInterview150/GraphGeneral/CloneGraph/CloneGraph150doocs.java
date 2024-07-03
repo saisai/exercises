9aaf5fe8f32a596c4ea328fb688f912f3789a44e
sai <hello@gmail.com>
2024-07-03 21:07:52 +0700

    hello

---
+++ algorithms/neetcode/algorithms/algorithms/src/main/java/com/mycompany/leetcode/TopInterview150/GraphGeneral/CloneGraph/CloneGraph150doocs.java
@@ -0,0 +1,63 @@
+package com.mycompany.leetcode.TopInterview150.GraphGeneral.CloneGraph;
+
+import com.mycompany.leetcode.TopInterview150.GraphGeneral.Nodes;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CloneGraph150mohamede1945 {
+    private Map<Nodes, Nodes> visited = new HashMap<>();
+
+    public Nodes cloneGraph(Nodes node) {
+        if (node == null) {
+            return null;
+        }
+        if (visited.containsKey(node)) {
+            return visited.get(node);
+        }
+        Nodes clone = new Nodes(node.val);
+        visited.put(node, clone);
+
+        for (Nodes e : node.neighbors) {
+            clone.neighbors.add(cloneGraph(e));
+        }
+        return clone;
+    }
+
+    public static void main(String[] args) {
+        Nodes node1 = new Nodes(1);
+        Nodes node2 = new Nodes(2);
+        Nodes node3 = new Nodes(3);
+        Nodes node4 = new Nodes(4);
+
+        node1.neighbors.add(node2);
+        node1.neighbors.add(node4);
+
+        node2.neighbors.add(node1);
+        node2.neighbors.add(node3);
+
+        node3.neighbors.add(node2);
+        node3.neighbors.add(node4);
+
+        node4.neighbors.add(node1);
+        node4.neighbors.add(node3);
+
+        CloneGraph150mohamede1945 obj = new CloneGraph150mohamede1945();
+        Nodes result = obj.cloneGraph(node1);
+        printGraph(result, new HashMap<>());
+
+    }
+
+    private static void printGraph(Nodes origin, Map<Integer, Nodes> used) {
+
+        if (!used.containsKey(origin.val)) {
+            System.out.println(origin.val);
+
+            used.put(origin.val, origin);
+            for (int i = 0; i < origin.neighbors.size(); i++) {
+                printGraph(origin.neighbors.get(i), used);
+            }
+        }
+    }
+
+}
